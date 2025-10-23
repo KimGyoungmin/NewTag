c63507be552a75e2a17c3177c0937329d0e736e8
unknown <min199485@gmail.com>
2025-10-23 18:09:24 +0900

    add : security/securityConfig.java

--- FrontEnd/NewTag/src/App.jsx
+++ FrontEnd/NewTag/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TestConnection from './TestConnection'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+
+      {/* CORS 연결 테스트 컴포넌트 */}
+      <TestConnection />
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
